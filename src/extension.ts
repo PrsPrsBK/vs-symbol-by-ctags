71f90bbbeab0d29b98ba4bcedd85273a6aaa0b3f
PrsPrsBK <prsprsbk@gmail.com>
2019-04-22 05:55:50 +0900

    add README

--- src/extension.ts
+++ src/extension.ts
@@ -4,20 +4,49 @@ import fs from 'fs';
 export function activate(context: vscode.ExtensionContext) {
   console.log('"symbol-by-ctags" is now active!');
 
-  const activateCommand = vscode.commands.registerCommand('extension.getSymbols', () => {
+  const activateCommand = vscode.commands.registerCommand('extension.symbolByCtags', () => {
     vscode.window.showInformationMessage('activated: Symbol by Ctags');
   });
   context.subscriptions.push(activateCommand);
 
-  context.subscriptions.push(
-    vscode.languages.registerDocumentSymbolProvider(
-      { pattern: '**/*.rst', scheme: 'file', },
-      new CtagsDocumentSymbolProvider()
-    )
-  );
+  // later config: SbcConfig or so
+  const config = vscode.workspace.getConfiguration('SymbolByCtags');
+  const documentFilterArray: vscode.DocumentFilter[] = [];
+  const targetArray = config.get<SbcTarget[]>('target');
+  if(targetArray !== undefined) {
+    console.log('yes');
+    for(const tgt of targetArray) {
+      console.log(JSON.stringify(tgt));
+      documentFilterArray.push({
+        pattern: tgt.glob,
+        scheme: 'file',
+      });
+    }
+  }
+  if(documentFilterArray.length > 0) {
+    console.log('register');
+    context.subscriptions.push(
+      vscode.languages.registerDocumentSymbolProvider(
+        documentFilterArray,
+        new CtagsDocumentSymbolProvider()
+      )
+    );
+  }
 }
 
 export function deactivate() { }
+
+interface SbcConfig extends vscode.WorkspaceConfiguration {
+  tags: string[];
+  target: SbcTarget[];
+}
+
+interface SbcTarget {
+  name: string;
+  glob: string;
+  exec: string;
+  tags: string;
+}
 
 export class CtagsDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
 
@@ -28,7 +57,8 @@ export class CtagsDocumentSymbolProvider implements vscode.DocumentSymbolProvide
     //'.' means directory of code.exe
     const fileName = document.uri.path.replace(/.*\/([^\/]+)/, '$1');
     const dirName = document.uri.fsPath.replace(fileName, '');
-    let wk = fs.existsSync(`${dirName}/.tags`);
+    if(fs.existsSync(`${dirName}/.tags`)) {
+    }
     const result: vscode.SymbolInformation[] = [];
     result.push(
       new vscode.SymbolInformation(
