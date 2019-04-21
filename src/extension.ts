4a56cb5dd1f167671a4dd011ed41ebe703f77273
PrsPrsBK <prsprsbk@gmail.com>
2019-04-21 23:40:31 +0900

    entry point

--- src/extension.ts
+++ src/extension.ts
@@ -4,10 +4,37 @@ export function activate(context: vscode.ExtensionContext) {
   console.log('"symbol-by-ctags" is now active!');
 
   let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-    vscode.window.showInformationMessage('HelloA World!');
+    vscode.window.showInformationMessage('HelloAA World!');
   });
 
   context.subscriptions.push(disposable);
+
+  context.subscriptions.push(
+    vscode.languages.registerDocumentSymbolProvider(
+      'somelang',
+      new CtagsDocumentSymbolProvider()
+    )
+  );
 }
 
 export function deactivate() { }
+
+export class CtagsDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
+
+  public provideDocumentSymbols(
+    document: vscode.TextDocument,
+    _token: vscode.CancellationToken): vscode.SymbolInformation[] {
+
+    const result: vscode.SymbolInformation[] = [];
+    result.push(
+      new vscode.SymbolInformation(
+        'foo_function',
+        vscode.SymbolKind.Function,
+        '',
+        new vscode.Location(document.uri, new vscode.Position(10, 0))
+      )
+    );
+
+    return result;
+  }
+}
