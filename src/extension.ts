d4e5076820577d84acd975a1ab045468b08102e7
PrsPrsBK <prsprsbk@gmail.com>
2019-04-29 04:33:33 +0900

    kind to SymbolKind mappings in settings

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import fs from 'fs';
 import readline from 'readline';
+import { rejects } from 'assert';
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('"symbol-by-ctags" is now active!');
@@ -21,14 +22,14 @@ export function activate(context: vscode.ExtensionContext) {
         scheme: 'file',
       });
     }
-  }
-  if(documentFilterArray.length > 0) {
-    context.subscriptions.push(
-      vscode.languages.registerDocumentSymbolProvider(
-        documentFilterArray,
-        new CtagsDocumentSymbolProvider()
-      )
-    );
+    if(documentFilterArray.length > 0) {
+      context.subscriptions.push(
+        vscode.languages.registerDocumentSymbolProvider(
+          documentFilterArray,
+          new CtagsDocumentSymbolProvider(targetArray)
+        )
+      );
+    }
   }
 }
 
@@ -42,11 +43,18 @@ interface SbcConfig extends vscode.WorkspaceConfiguration {
 interface SbcTarget {
   name: string;
   glob: string;
+  ends: string[];
+  kindMap: any;
   exec: string;
   tags: string[];
 }
 
 export class CtagsDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
+  configArray: Array<SbcTarget>;
+
+  constructor(configArray: Array<SbcTarget>) {
+    this.configArray = configArray;
+  }
 
   public provideDocumentSymbols(
     document: vscode.TextDocument,
@@ -55,9 +63,14 @@ export class CtagsDocumentSymbolProvider implements vscode.DocumentSymbolProvide
     //'.' means directory of code.exe
     const fileName = document.uri.path.replace(/.*\/([^\/]+)/, '$1');
     const dirName = document.uri.fsPath.replace(fileName, '');
+    const config = this.configArray.find(aConfig => {
+      const wk = aConfig.ends.find(nameEnd => { return fileName.endsWith(nameEnd); });
+      return wk !== undefined;
+    });
     const result: vscode.SymbolInformation[] = [];
     return new Promise<vscode.SymbolInformation[]>((resolve, reject) => {
       if(fs.existsSync(`${dirName}/.tags`)) {
+        const kindMap = config !== undefined ? config.kindMap : undefined;
         const rs = fs.createReadStream(`${dirName}/.tags`);
         const lines = readline.createInterface(rs);
         lines.on('line', line => {
@@ -67,11 +80,86 @@ export class CtagsDocumentSymbolProvider implements vscode.DocumentSymbolProvide
               ? parseInt(tokens[4].split(':')[1]) // lines:n
               : parseInt(tokens[2].replace(';"', '')); // nn;"
             let kind = vscode.SymbolKind.Constructor;
-            if(tokens[3] === 'f') {
-              kind = vscode.SymbolKind.Function;
-            }
-            if(tokens[3] === 'c') {
-              kind = vscode.SymbolKind.Class;
+            if(kindMap !== undefined && kindMap[tokens[3]] !== undefined) {
+              const kindSpec = kindMap[tokens[3]];
+              if(kindSpec === 'Array') {
+                kind = vscode.SymbolKind.Array;
+              }
+              else if(kindSpec === 'Boolean') {
+                kind = vscode.SymbolKind.Boolean;
+              }
+              else if(kindSpec === 'Class') {
+                kind = vscode.SymbolKind.Class;
+              }
+              else if(kindSpec === 'Constant') {
+                kind = vscode.SymbolKind.Constant;
+              }
+              else if(kindSpec === 'Constructor') {
+                kind = vscode.SymbolKind.Constructor;
+              }
+              else if(kindSpec === 'Enum') {
+                kind = vscode.SymbolKind.Enum;
+              }
+              else if(kindSpec === 'EnumMember') {
+                kind = vscode.SymbolKind.EnumMember;
+              }
+              else if(kindSpec === 'Event') {
+                kind = vscode.SymbolKind.Event;
+              }
+              else if(kindSpec === 'Field') {
+                kind = vscode.SymbolKind.Field;
+              }
+              else if(kindSpec === 'File') {
+                kind = vscode.SymbolKind.File;
+              }
+              else if(kindSpec === 'Function') {
+                kind = vscode.SymbolKind.Function;
+              }
+              else if(kindSpec === 'Interface') {
+                kind = vscode.SymbolKind.Interface;
+              }
+              else if(kindSpec === 'Key') {
+                kind = vscode.SymbolKind.Key;
+              }
+              else if(kindSpec === 'Method') {
+                kind = vscode.SymbolKind.Method;
+              }
+              else if(kindSpec === 'Module') {
+                kind = vscode.SymbolKind.Module;
+              }
+              else if(kindSpec === 'Namespace') {
+                kind = vscode.SymbolKind.Namespace;
+              }
+              else if(kindSpec === 'Null') {
+                kind = vscode.SymbolKind.Null;
+              }
+              else if(kindSpec === 'Number') {
+                kind = vscode.SymbolKind.Number;
+              }
+              else if(kindSpec === 'Object') {
+                kind = vscode.SymbolKind.Object;
+              }
+              else if(kindSpec === 'Operator') {
+                kind = vscode.SymbolKind.Operator;
+              }
+              else if(kindSpec === 'Package') {
+                kind = vscode.SymbolKind.Package;
+              }
+              else if(kindSpec === 'Property') {
+                kind = vscode.SymbolKind.Property;
+              }
+              else if(kindSpec === 'String') {
+                kind = vscode.SymbolKind.String;
+              }
+              else if(kindSpec === 'Struct') {
+                kind = vscode.SymbolKind.Struct;
+              }
+              else if(kindSpec === 'TypeParameter') {
+                kind = vscode.SymbolKind.TypeParameter;
+              }
+              else if(kindSpec === 'Variable') {
+                kind = vscode.SymbolKind.Variable;
+              }
             }
             result.push(
               new vscode.SymbolInformation(
