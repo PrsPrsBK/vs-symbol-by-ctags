793ebc85e4b8792785f791172b0b4534a17b9d9d
PrsPrsBK <prsprsbk@gmail.com>
2019-06-01 02:07:52 +0900

    refactor and fix. case of usual workspace folder

--- src/extension.ts
+++ src/extension.ts
@@ -180,7 +180,7 @@ const getParentFixedTagsInfo = (docFilePath: string) => {
 };
 
 const normalizePathAsKey = (filePath: string): string => {
-  return filePath.replace(/^\/?([A-Z]){1}:\//, (_match, p1) => {
+  return filePath.replace(/^\/?([A-Za-z]){1}:\//, (_match, p1) => {
     return `${p1.toLowerCase()}:/`;
   });
 };
@@ -248,12 +248,12 @@ const getDocumentSymbols = (document: vscode.TextDocument): Promise<vscode.Docum
   else {
     console.log(`wf: ${wf.uri.path}`);
   }
+  // On win32, you get path such as /x:/path/to/folder
   const docFilePath = normalizePathAsKey(document.uri.path);
   if(getParentFixedTagsPath(docFilePath) !== undefined) {
     console.log('---- INSIDE FIXEDTAGS ----');
     const fixedTagsInfo = getParentFixedTagsInfo(docFilePath);
     if(fixedTagsInfo !== undefined && fixedTagsInfo.docSymbolMap !== undefined) {
-      // anyway Typescript requires workaround variable.
       const workaroundVar = fixedTagsInfo.docSymbolMap.get(docFilePath);
       if(workaroundVar !== undefined) {
         return Promise.resolve(workaroundVar);
@@ -264,14 +264,11 @@ const getDocumentSymbols = (document: vscode.TextDocument): Promise<vscode.Docum
     }
   }
 
-  // On win32, you get path such as /x:/path/to/folder
-  const sliceFrom = os.platform() === 'win32' ? 1 : 0;
-  const docDirFsPath = document.uri.path.replace(/(.+)\/[^\/]+$/, '$1').slice(sliceFrom);
-
+  const docDirFsPath = docFilePath.replace(/(.+)\/[^\/]+$/, '$1');
   let tagsFileName = tagsFileList.find(f => fs.existsSync(`${docDirFsPath}/${f}`));
   let tagsDirFsPath = docDirFsPath;
+  const wfFsPath = normalizePathAsKey(wf.uri.path);
   if(tagsFileName === undefined) {
-    const wfFsPath = wf.uri.path.slice(sliceFrom);
     while(true) {
       if(tagsDirFsPath === wfFsPath || tagsFileName !== undefined) {
         break;
@@ -281,24 +278,27 @@ const getDocumentSymbols = (document: vscode.TextDocument): Promise<vscode.Docum
     }
   }
   if(tagsFileName === undefined) {
+    // notification may be over-reaction
     console.error('tags file not found.');
     return Promise.reject([]);
   }
 
+  const tagsPath = `${tagsDirFsPath}/${tagsFileName}`;
+  if(fs.existsSync(tagsPath) === false) {
+    // return Promise.reject(`NOT EXIST ${tagsPath}`);
+    console.log(`NOT EXIST ${tagsPath}`);
+    return Promise.reject([]);
+  }
   const lastMtimeMs = fs.statSync(`${tagsDirFsPath}/${tagsFileName}`).mtimeMs;
-  console.log(JSON.stringify(lastMtimeMs)); //1558774393762.249
 
   // liveWsInfo is just a workaround.
   // I do not know why tsc ignores '=== undefined' and re-assigning to curWsInfo,
   // and eventually results in failure to compile.
-  let liveWsInfo = allWorkspace.get(wf.uri.path);
+  let liveWsInfo = allWorkspace.get(wfFsPath);
   // curWsInfo already exists, so let's see.
   if(liveWsInfo !== undefined) {
     if(liveWsInfo.mstimeMs === lastMtimeMs) { // NOT modified!
-      const workaroundPath = document.uri.path.replace(/^\/([A-Z]):\//, (_match, p1) => {
-        return `/${p1.toLowerCase()}:/`;
-      })
-      const liveDocumentSymbols = liveWsInfo.docSymbolMap.get(workaroundPath);
+      const liveDocumentSymbols = liveWsInfo.docSymbolMap.get(docFilePath);
       if(liveDocumentSymbols !== undefined) {
         return Promise.resolve(liveDocumentSymbols);
       }
@@ -315,210 +315,11 @@ const getDocumentSymbols = (document: vscode.TextDocument): Promise<vscode.Docum
     docSymbolMap: new Map<string, vscode.DocumentSymbol[]>(),
     wsSymbolArray: [],
   } as eachWorkspace;
-  allWorkspace.set(wf.uri.path, curWsInfo);
-
-  let eachFileResult: vscode.DocumentSymbol[] = [];
-  let currentTreeTop = '';
-  let parentArray: [string, number][] = [];
-  let lastFileNameInTokens = '';
-  let lastFileUriInTokens: vscode.Uri;
-  let wkConfig: SbcTarget;
-
-  const endOfSameFile = (docUri: vscode.Uri) => {
-    if(wkConfig.offSideRule && parentArray.length > 0) {
-      while(true) {
-        if(parentArray.length === 0) {
-          break;
-        }
-        const last = parentArray[parentArray.length - 1];
-        let parent: (vscode.DocumentSymbol | undefined) = undefined;
-        for(const ancestor of parentArray) {
-          if(parent === undefined) { // 1st ansector
-            parent = eachFileResult.find(docSym => docSym.name === ancestor[0]);
-          }
-          else {
-            parent = parent.children.find(docSym => docSym.name === ancestor[0]);
-          }
-          if(parent === undefined) { // failed one
-            break;
-          }
-        }
-        if(parent === undefined) {
-          console.error(`${new Date().toLocaleTimeString()} ERROR: ${last[0]}: at symbol hierarchy spec within tags file`);
-          parentArray = [];
-        }
-        else {
-          parent.range = parent.range.with({end: new vscode.Position(document.lineCount - 1, 0)});
-          parentArray.pop();
-        }
-      }
-    }
-    curWsInfo.docSymbolMap.set(docUri.path, eachFileResult);
-  };
+  allWorkspace.set(wfFsPath, curWsInfo);
 
   return new Promise<vscode.DocumentSymbol[]>((resolve, _reject) => {
-    const rs = fs.createReadStream(`${tagsDirFsPath}/${tagsFileName}`);
-    const lines = readline.createInterface(rs);
-
-    lines.on('line', line => {
-      if(line.startsWith('!_TAG_')) {
-        return;
-      }
-
-      // currently read all lines. if 'not sorted by symbolname', to stop readline is better.
-      const tokens = line.split('\t');
-
-      const symbolName = tokens[0];
-      // On Windows, spec within tags file may have paths separated by backslash.
-      const fileNameInTokens = tokens[1].replace(/\\/g, '/');
-      // Maybe it is better to validate path.
-      const fileUriInTokens = vscode.Uri.file(`${tagsDirFsPath}/${fileNameInTokens}`);
-
-      if(lastFileNameInTokens === '') {
-        lastFileNameInTokens = fileNameInTokens;
-        lastFileUriInTokens = fileUriInTokens;
-        wkConfig = getCleanConfig(fileUriInTokens);
-      }
-      else if(fileNameInTokens !== lastFileNameInTokens) {
-        endOfSameFile(lastFileUriInTokens);
-        lastFileNameInTokens = fileNameInTokens;
-        lastFileUriInTokens = fileUriInTokens;
-        wkConfig = getCleanConfig(fileUriInTokens);
-        eachFileResult = [];
-      }
-
-      let kind = vscode.SymbolKind.Constructor; // no reason for Constructor
-      if(wkConfig.kindMap[tokens[3]] !== undefined
-        && kind2SymbolKind[wkConfig.kindMap[tokens[3]]] !== undefined) {
-        kind = kind2SymbolKind[wkConfig.kindMap[tokens[3]]];
-      }
-      const posLine = tokens.length > 4
-        ? parseInt(tokens[4].split(':')[1]) // lines:n
-        : parseInt(tokens[2].replace(';"', '')); // nn;"
-      const innerRegex = tokens[2].startsWith('/') // /^  foo$/;"
-        ? tokens[2].slice(2, tokens[2].length - 4)
-        : '';
-      const posCol = (tokens[2].startsWith('/') && innerRegex.indexOf(symbolName) !== -1)
-        ? innerRegex.indexOf(symbolName)
-        : 0;
-      const symbolNameRange = new vscode.Range(posLine - 1, posCol, posLine - 1, posCol + symbolName.length);
-
-      curWsInfo.wsSymbolArray.push(
-        new vscode.SymbolInformation(
-          symbolName,
-          kind,
-          fileNameInTokens,
-          new vscode.Location(fileUriInTokens, symbolNameRange)
-        )
-      );
-
-      let workSymbolRanges = curWsInfo.docRangeMap.get(fileUriInTokens.path);
-      if(workSymbolRanges === undefined) {
-        workSymbolRanges = [];
-        curWsInfo.docRangeMap.set(fileUriInTokens.path, workSymbolRanges);
-      }
-      workSymbolRanges.push(symbolNameRange);
-
-      const currentSymbol = new vscode.DocumentSymbol(
-        symbolName,
-        '',
-        kind,
-        new vscode.Range(posLine - 1, 0, posLine, 10), // 'posLine, 10' has no meaning
-        symbolNameRange
-      );
-      currentSymbol.children = [];
-
-      const indentRegex = /^[ ]+/g;
-      if(wkConfig.offSideRule && innerRegex !== '') {
-        const curIndent = indentRegex.exec(innerRegex) !== null
-          ? indentRegex.lastIndex : 0;
-        while(true) {
-          if(parentArray.length === 0) {
-            eachFileResult.push(currentSymbol);
-            parentArray.push([ symbolName, curIndent ]);
-            break;
-          }
-          const last = parentArray[parentArray.length - 1];
-          let parent: (vscode.DocumentSymbol | undefined) = undefined;
-          for(const ancestor of parentArray) {
-            if(parent === undefined) { // 1st ansector
-              parent = eachFileResult.find(docSym => docSym.name === ancestor[0]);
-            }
-            else {
-              parent = parent.children.find(docSym => docSym.name === ancestor[0]);
-            }
-            if(parent === undefined) { // failed one
-              break;
-            }
-          }
-          if(parent === undefined) {
-            console.error(`${new Date().toLocaleTimeString()} ERROR: ${symbolName}: at symbol hierarchy spec within tags file`);
-            eachFileResult.push(currentSymbol);
-            // when failed to get parent symbol obj, there may be better way to go,
-            // but I do not know now.
-            parentArray = [];
-          }
-          else if(last[1] < curIndent) {
-            parent.children.push(currentSymbol);
-            parentArray.push([ symbolName, curIndent ]);
-            break;
-          }
-          else {
-            parent.range = parent.range.with({end: new vscode.Position(posLine - 1, 0)});
-            parentArray.pop();
-          }
-        }
-      }
-      else if(wkConfig.restartTree !== '') {
-        if(wkConfig.restartTree.includes(tokens[3])) {
-          eachFileResult.push(currentSymbol);
-          currentTreeTop = symbolName;
-        }
-        else if(currentTreeTop === '') {
-          eachFileResult.push(currentSymbol);
-        }
-        else {
-          const parent = eachFileResult.find(docSym => docSym.name === currentTreeTop);
-          if(parent !== undefined) {
-            parent.children.push(currentSymbol);
-          }
-          else {
-            eachFileResult.push(currentSymbol);
-          }
-        }
-      }
-      // case of rst2ctags.py
-      // tokens[5] takes form of 'section:foo|bar...'
-      else if(tokens.length > 5 && tokens[5] !== '' && wkConfig.sro !== '') {
-        let parent: (vscode.DocumentSymbol | undefined) = undefined;
-        for(const ancestor of tokens[5].slice(1 + tokens[5].indexOf(':')).split(wkConfig.sro)) {
-          if(parent === undefined) { // 1st ansector
-            parent = eachFileResult.find(docSym => docSym.name === ancestor);
-          }
-          else {
-            parent = parent.children.find(docSym => docSym.name === ancestor);
-          }
-          if(parent === undefined) { // failed one
-            break;
-          }
-        }
-        if(parent === undefined) {
-          console.error(`${new Date().toLocaleTimeString()} ERROR: ${symbolName}: at symbol hierarchy spec within tags file`);
-          eachFileResult.push(currentSymbol);
-        }
-        else {
-          parent.children.push(currentSymbol);
-        }
-      }
-      else {
-        eachFileResult.push(currentSymbol);
-      }
-    });
-
-    lines.on('close', () => {
-      endOfSameFile(lastFileUriInTokens);
-      rs.destroy();
-      resolve(curWsInfo.docSymbolMap.get(document.uri.path));
+    buildSub(tagsPath, curWsInfo).then(_result => {
+      resolve(curWsInfo.docSymbolMap.get(docFilePath));
     });
   });
 };
@@ -582,7 +383,10 @@ const buildSub = (tagsPath: string, curWsInfo: eachWorkspace) => {
         }
       }
     }
-    curWsInfo.docSymbolMap.set(docUri.path, eachFileResult);
+    curWsInfo.docSymbolMap.set(normalizePathAsKey(docUri.path), eachFileResult);
+    if(docUri.path.includes('ponyc/') === false) {
+      console.log(`  END ${lastLineNum} ${docUri.path}`);
+    }
   };
   const rs = fs.createReadStream(`${tagsPath}`);
   const lines = readline.createInterface(rs);
@@ -600,17 +404,22 @@ const buildSub = (tagsPath: string, curWsInfo: eachWorkspace) => {
       // On Windows, spec within tags file may have paths separated by backslash.
       const fileNameInTokens = tokens[1].replace(/\\/g, '/');
       // Maybe it is better to validate path.
+      // if(tagsDirFsPath.includes('ponyc') === false) {
+      //   console.log(`${tagsDirFsPath}/${fileNameInTokens}`);
+      // }
       const fileUriInTokens = vscode.Uri.file(`${tagsDirFsPath}/${fileNameInTokens}`);
 
       if(lastFileNameInTokens === '') {
         lastFileNameInTokens = fileNameInTokens;
         lastFileUriInTokens = fileUriInTokens;
+        lastLineNum = 0
         wkConfig = getCleanConfig(fileUriInTokens);
       }
       else if(fileNameInTokens !== lastFileNameInTokens) {
         endOfSameFile(lastFileUriInTokens);
         lastFileNameInTokens = fileNameInTokens;
         lastFileUriInTokens = fileUriInTokens;
+        lastLineNum = 0
         wkConfig = getCleanConfig(fileUriInTokens);
         eachFileResult = [];
       }
@@ -623,6 +432,10 @@ const buildSub = (tagsPath: string, curWsInfo: eachWorkspace) => {
       const posLine = tokens.length > 4
         ? parseInt(tokens[4].split(':')[1]) // lines:n
         : parseInt(tokens[2].replace(';"', '')); // nn;"
+      lastLineNum = posLine;
+      if(tagsDirFsPath.includes('ponyc') === false) {
+        console.log(`${fileNameInTokens} ${lastLineNum}`);
+      }
       const innerRegex = tokens[2].startsWith('/') // /^  foo$/;"
         ? tokens[2].slice(2, tokens[2].length - 4)
         : '';
